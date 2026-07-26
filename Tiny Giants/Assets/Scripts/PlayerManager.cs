e17d7b2b390af790224ac20d932436118f5da4c7
lentilek <laura.robinska@wp.pl>
2026-07-26 14:32:45 +0200

    Player movement

---
+++ Tiny Giants/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager Instance;
+
+    private Rigidbody rb;
+    //[SerializeField] private SpriteRenderer charSprite;
+    //[SerializeField] private Animator animator;
+    [SerializeField] private float playerSpeed;
+    public Vector3 playerHomePoint;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(Instance.gameObject);
+            Instance = this;
+        }
+    }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //gameObject.transform.position = playerHomePoint;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.forward * playerSpeed * Time.deltaTime;
+
+            // sprite front
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.back * playerSpeed * Time.deltaTime;
+
+            // sprite back
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.right * playerSpeed * Time.deltaTime;
+
+            //charSprite.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position -= Vector3.right * playerSpeed * Time.deltaTime;
+
+            //charSprite.flipX = true;
+        }
+
+        // idle state
+    }
+}
